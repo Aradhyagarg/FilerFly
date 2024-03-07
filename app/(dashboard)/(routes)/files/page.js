e2dfa65123db768fc24b5efa28b7105923041a2f
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2024-03-08 00:43:09 +0530

    Home and SignUp

---
+++ app/(dashboard)/(routes)/files/page.js
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function Files() {
+  return (
+    <div className='text-red-700'>
+        Files
+        <UserButton />
+    </div>
+  )
+}
+
+export default Files
---
+++ app/_utils/Constant.js
@@ -0,0 +1,3 @@
+export default{
+    desc: 'Drag and drop your file directly on our cloud and share it with your friends secuarely with the password and send it on email',
+}
