c50ee4c8d9766fbeeb3bfcb0e6e250c3b72dc7b5
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2024-03-10 19:30:51 +0530

    show file view and delete the file

--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AlertMsg from './AlertMsg';
+import FilePreview from './FilePreview';
 
 function UploadForm() {
     const [file, setFile] = useState();
@@ -33,6 +34,7 @@ function UploadForm() {
                 </label>
             </div>
             {error ? <AlertMsg msg={error}/> : null}
+            {file ? <FilePreview file={file} removeFile={() => setFile(null)}/> : null}
             <button disabled={!file} className="p-2 bg-primary text-white w-[30%] rounded-full mt-5 disabled:bg-gray-500">Upload</button>
         </div>
     );
